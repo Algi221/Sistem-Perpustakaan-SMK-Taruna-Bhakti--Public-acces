1b9f2f98e5ced77579e532b7fbce44f139f6aa6b
Algi221 <algifahrihero.04@gmail.com>
2025-12-08 09:55:45 +0700

    Fix: Add Providers to layout and dynamic export to login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,6 +1,8 @@
 import LoginLayout from './components/LoginLayout';
 import LoginForm from './components/LoginForm';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return (
     <LoginLayout>
